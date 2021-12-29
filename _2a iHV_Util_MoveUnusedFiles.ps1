b74fafe25a7c5c7f5c25de50ab10bb61fbeb3928
Bobb <95751916+BoominBobbyBo@users.noreply.github.com>
2021-12-29 01:56:21 -0500

    Removed false positives in the orphaned list
    Special chars in the file names caused referenced files to appear in the non referenced file list. Added a regex escape to resolve.

--- _2a iHV_Util_MoveUnusedFiles.ps1
+++ _2a iHV_Util_MoveUnusedFiles.ps1
@@ -34,21 +34,38 @@ Read-Host -Prompt 'No. Really. All morphs, hair, and clothes will be moved if yo
 If($vamRoot -eq $null){ $vamRoot = ($PSScriptRoot + '\') }
 
 $ScriptName = 'iHV_Util_MoveUnusedFiles'
-$ScriptVersion = '0.5'
+$ScriptVersion = '1.0.1'
 $LogPath = ($PSScriptRoot + '\_2a ' + $ScriptName + '.log')
 $LogEntry = Get-Date -Format 'yyyy/MM/dd HH:mm' 
 
-$blnBuildLinkLibrary = $true   # set to false to skip the process of scanning all instruction files and leverage a previous LinksCSV.csv build 
-                               # CAUTION: running as false witout a previous and currents LinksCSV.csv  will cause all morphs, hair and cloths to be moved
+$blnBuildLinkLibrary = $true     # set to false to skip the process of scanning all instruction files and leverage a previous LinksCSV.csv build 
+                                 # CAUTION: running as false witout a previous and currents LinksCSV.csv  will cause all morphs, hair and cloths to be moved
 
-$blnMoveMorphs = $true         # set to false to block action but still produce a log/report
-$blnMoveHair = $false          # set to false to block action but still produce a log/report
-$blnMoveClothing = $false      # set to false to block action but still produce a log/report
-$blnMoveTextures = $true       # set to false to block action but still produce a log/report
+$blnMoveMorphs       = $true     # set to false to block action but still produce a log/report
+$blnMoveHair         = $false    # set to false to block action but still produce a log/report
+$blnMoveClothing     = $false    # set to false to block action but still produce a log/report
+$blnMoveTextures     = $false    # set to false to block action but still produce a log/report
+$blnMoveAudio        = $false    # set to false to block action but still produce a log/report
 
 $RecycleBin          = ($vamRoot + '_2a iHV_RecycleBin')
 $LinksCSVpath        = ($RecycleBin + '\_2a iHV_InstructionLinks.csv')
 $MoveReportCSVpath   = ($RecycleBin + '\_2a iHV_MoveReport.csv')
+
+$blnDebug            = $false     # adds source file to the links file (which doubles it size)
+
+
+# CONTENT FOLDERS -  directories with content files with pathing that needs to be updated
+$InstructionsDirs = @(
+    "Custom\Assets\"
+    "Custom\Atom\Person\"
+    "Custom\Clothing\"
+    "Custom\Hair\"
+    "Custom\Images\"
+    "Custom\SubScene\"
+    "Custom\Sounds\"
+    "Saves\Person\"
+    "Saves\scene\"
+)
 
 
     ###################### SCRIPT TUNING ###################### 
@@ -58,55 +75,57 @@ MD ($RecycleBin + "\Clothing") -ErrorAction SilentlyContinue
 MD ($RecycleBin + "\Hair") -ErrorAction SilentlyContinue
 MD ($RecycleBin + "\Morph") -ErrorAction SilentlyContinue
 MD ($RecycleBin + "\Images") -ErrorAction SilentlyContinue
+MD ($RecycleBin + "\Audio") -ErrorAction SilentlyContinue
 
 """"+'Type'+""""+','+""""+ 'FullFilePath' +"""" | Out-File -FilePath $MoveReportCSVpath # Clears previous and creates the report file anew for each execution
-
 
 # Scan VAM instruction files to build a live database of required resource files (aka 'links')
 
 If($blnBuildLinkLibrary -eq $true){
 
-    """"+'Link_RelPath' +"""" | Out-File -FilePath $LinksCSVpath # Clears previous and creates the library file anew for each execution
+    IF($blnDebug -eq $true){ """"+'Link_RelPath' +""""+','+""""+ 'Source' +"""" | Out-File -FilePath $LinksCSVpath } # Clears previous and creates the library file anew for each execution
+    Else {""""+'Link_RelPath' +"""" | Out-File -FilePath $LinksCSVpath }
 
     $arrInstructionLinks = @()
-    Get-ChildItem -Path $vamRoot -File -include *.json, *.vap, *.vaj -Recurse -Force | Foreach-Object { 
+    $InstructionsDirs | ForEach-Object {
 
+        Get-ChildItem -Path ($vamRoot + $_)  -File -include *.json, *.vap, *.vaj -Recurse -Force | Foreach-Object { 
+    
+            $SourceFullFilePath = $_.FullName
+            $Directory = $_.Directory
 
-        $FullFilePath = $_.FullName
-        $Directory = $_.Directory
+            Write-Host ---Reading: $SourceFullFilePath
 
-        Write-Host ---Reading: $FullFilePath
+            $Instructions = [System.IO.File]::ReadAllLines($_) | Where-Object { $_ -inotmatch (""""+"name"+""""+":") -and ($_ -ilike "*Custom/*" -or $_ -ilike "*Saves/*") -and ($_ -imatch ".vam" -or $_ -imatch ".vmi" -or $_ -imatch ".jpg" -or $_ -imatch ".png" -or $_ -imatch ".mp3" -or $_ -imatch ".wav" -or $_ -imatch ".ogg") }
 
-        If( ($blnMoveHair -eq $true -or $blnMoveClothing -eq $true) -and $blnMoveTextures -eq $true ){ $Instructions = [System.IO.File]::ReadAllLines($_) | Where-Object { $_ -imatch ".vmi" -or $_ -imatch ".vam" -or $_ -imatch ".jpg" -or $_ -imatch ".png" } }
-        ElseIf($blnMoveHair -eq $true -or $blnMoveClothing -eq $true ){ $Instructions = [System.IO.File]::ReadAllLines($_)| Where-Object { $_ -imatch ".vmi" -or $_ -imatch ".vam"} }
-        ElseIf($blnMoveTextures -eq $true ){ $Instructions = [System.IO.File]::ReadAllLines($_) | Where-Object { $_ -imatch ".vmi" -or $_ -imatch ".jpg" -or $_ -imatch ".png" } }
-        Else{ $Instructions = [System.IO.File]::ReadAllLines($_) | Where-Object { $_ -imatch ".vmi" -or $_ -imatch ".jpg" -or $_ -imatch ".png" } }
+            $Instructions | ForEach-Object { 
 
-        $Instructions | ForEach-Object { 
+                $NodeName = ''
+                $Link_FolderPath = ''
+                $LastSlash = 0
+                $Link_FileName = ''
+                $Link_RelPath = ''
 
-            $NodeName = ''
-            $Link_FolderPath = ''
-            $LastSlash = 0
-            $Link_FileName = ''
-            $Link_RelPath = ''
+                $NodeName = $_.Substring(0, $_.indexOf(':') + 3 ).Trim() # json node value pair - node, e.g. 'ID : '
 
-            $NodeName = $_.Substring(0, $_.indexOf(':') + 3 ).Trim() # json node value pair - node, e.g. 'ID : '
+                If($NodeName.Length -ge 1){
+                    $Link_RelPath = $_.Replace($NodeName,'').Trim().Trim(',').Trim("""")  # json node value pair - value, e.g. 'Custom/sounds/some.mp3'
 
-            If($NodeName.Length -ge 1){
-                $Link_RelPath = $_.Replace($NodeName,'').Trim().Trim(',').Trim("""")  # json node value pair - value, e.g. 'Custom/sounds/some.mp3'
+                    # if file name only, add relative path or the check below will show a false positive
+                    If($Link_RelPath.IndexOf("/") -eq -1){$Link_RelPath = ($Directory.ToString().Replace("\","/") + "/" + $Link_RelPath)}
 
-                # if file name only, add relative path or the check below will show a false positive
-                If($Link_RelPath.IndexOf("/") -eq -1){$Link_RelPath = ($Directory.ToString().Replace("\","/") + "/" + $Link_RelPath)}
+                    If($Link_RelPath.Length -ge 18){ # sample: Custom/890/234.578
 
-                If($Link_RelPath.Length -ge 18){ # sample: Custom/890/234.578
-
-                    """"+$Link_RelPath +"""" | Out-File -FilePath $LinksCSVpath -Append
+                        If($blnDebug -eq $true){ """"+$Link_RelPath +""""+','+""""+ $SourceFullFilePath +"""" | Out-File -FilePath $LinksCSVpath -Append }
+                        Else{ """"+$Link_RelPath +"""" | Out-File -FilePath $LinksCSVpath -Append }
+                    }
                 }
-            }
 
-        } # $Instructions '/'
+            } # $Instructions '/'
 
-    } # Get-ChildItem vamRoot files
+        } # Get-ChildItem Custom files
+    
+    } # For each InstructionDir
 
 } # If($blnBuildLinkLibrary -eq $true){""""+'Link_RelPath' +"""" | Out-File -FilePath $LinksCSVpath}
 
@@ -119,20 +138,17 @@ Write-Host ...Loaded: $LinksCSV.Count resource links
 #      - using explicit paths to avoid scanning \Scripts, etc.
 
 $arrResourceFiles = @()
-Get-ChildItem -Path ($vamRoot + 'Custom\Assets' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
-Get-ChildItem -Path ($vamRoot + 'Custom\Atom' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
-Get-ChildItem -Path ($vamRoot + 'Custom\Clothing' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
-Get-ChildItem -Path ($vamRoot + 'Custom\Hair' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
-Get-ChildItem -Path ($vamRoot + 'Custom\Images' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
-Get-ChildItem -Path ($vamRoot + 'Custom\SubScene' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
-Get-ChildItem -Path ($vamRoot + 'Saves\Person' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
-Get-ChildItem -Path ($vamRoot + 'Saves\scene' ) -File -Include *.vmi, *.vam, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
+$InstructionsDirs | ForEach-Object {
+
+    Get-ChildItem -Path ($vamRoot + $_)  -File -Include *.vmi, *.vam, *.jpg, *.png, *.mp3, *.wav, *.ogg -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -inotlike ('*' + $RecycleBin +'*')} | Foreach-Object { $arrResourceFiles += $_.FullName }
+
+}
 
 Write-Host ...Found: $arrResourceFiles.Count Custom and Saves files.
 
 
 
-# Compare the to tables to identify 
+# Compare the two tables to identify 
 #        missing and 
 #        unused resource files
 #        Optionally: Remove unused files
@@ -148,11 +164,11 @@ $arrResourceFiles | ForEach-Object {
     # For each resource file
 
     $File_FullPath     = $_ # Sample: C:\VAM\Custom\Sounds\boomboom.mp3
-    $File_VAMpath      = $File_FullPath.Replace($vamRoot,'').Replace('\','/') # sample: Custom/Sounds/boomboom.mp3
+    $File_VAMpath      = [regex]::Escape( $File_FullPath.Replace($vamRoot,'').Replace('\','/') ) # sample: Custom/Sounds/boomboom.mp3
 
     # Find resource file in the instructions path
 
-    if ($LinksCSV -imatch $File_VAMpath){}
+    if ($LinksCSV -imatch [regex]::Escape($File_VAMpath)){}
     Else{
         # if not in InstructionLinks....
 
@@ -163,11 +179,7 @@ $arrResourceFiles | ForEach-Object {
         
             If($File_VAMpath  -imatch '/hair/'){ $type = 'Hair' }
             ElseIf($File_VAMpath -imatch '/Clothing/'){ $type = 'Clothing' }       
-            Else{ 
-                Write-Host ---Unlinked resource found outside of hair or clothing paths. Potential idiot content: $File_VAMpath
-                $LogEntry + "---Unlinked resource found outside of hair|clothing path. Potential 'idiot' content. " +  $File_VAMpath | Out-File -FilePath $LogPath -Append
-                Return
-            }
+            Else{}
 
             # .vab, vaj and image files are not called directly by instruction files but they're in the filesystem
             # .vap and subfiles have prefixes; get child items with similar names to capture these
@@ -176,27 +188,25 @@ $arrResourceFiles | ForEach-Object {
 
             Get-ChildItem -Path $Path -File -Include *.vam, *.vab, *.vaj, *.vap, *.jpg, *.png -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {$_ -ilike ( $File_FullPath.Replace(".vam", "") + "*" )} | Foreach-Object {
 
-                write-host ---Moving $type ::: $_
+                write-host ---Orphaned $type : $_
+                """"+$type+""""+','+""""+ $_ +"""" | Out-File -FilePath $MoveReportCSVpath -Append
                        
                 If( ($blnMoveHair -eq $true -and $File_VAMpath -ilike '*/hair/*') -or ($blnMoveClothing -eq $true -and $File_VAMpath -ilike '*/Clothing/*') ){
 
                     $Error.Clear()
 
                     $_ | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue
-                    If($Error[0] -notmatch "because it does not exist."){ 
-                        """"+$type+""""+','+""""+ $_ +"""" | Out-File -FilePath $MoveReportCSVpath -Append
-                        $UnusedFileCount = $UnusedFileCount + 1
-                    }
+                    If($Error[0] -notmatch "because it does not exist."){ $UnusedFileCount = $UnusedFileCount + 1 }
                
                 } # If( ($blnMoveHair -eq $true -and $File_FullPath -ilike '*\hair\*')
 
             } # get child items 
 
-        } # if jpg or png
+        } # if vam
         ElseIf($File_VAMpath -ilike '*.vmi'){ 
 
             # Morphs
-            write-host ---Moving Morph: $File_VAMpath
+            write-host ---Orphaned Morph: $File_VAMpath
 
             # .VMB files are not called directly by instruction files, assume .VMI + .VMB
 
@@ -204,43 +214,40 @@ $arrResourceFiles | ForEach-Object {
             $jpg = $File_FullPath -iReplace('.vmi', '.jpg')
             $png = $File_FullPath -iReplace('.vmi', '.png')
             
-            If($blnMoveMorphs -eq $true){
-                $type = "Morph"
-                $Error.Clear()
+            $type = "Morph"
+            $Error.Clear()
                 
-                $File_FullPath | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue
-                If($Error[0] -notmatch "because it does not exist."){ 
+            If($blnMoveMorphs -eq $true){ $File_FullPath | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue }
+            If($Error[0] -notmatch "because it does not exist."){ 
                     """"+$type+""""+','+""""+ $File_FullPath +"""" | Out-File -FilePath $MoveReportCSVpath -Append
                     $UnusedFileCount = $UnusedFileCount + 1
-                 }
+            }
 
-                $Error.Clear()
+            $Error.Clear()
 
-                $vmb | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue
-                If($Error[0] -notmatch "because it does not exist."){ 
+            If($blnMoveMorphs -eq $true){ $vmb | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue }
+            If($Error[0] -notmatch "because it does not exist."){ 
                     """"+$type+""""+','+""""+ $vmb +"""" | Out-File -FilePath $MoveReportCSVpath -Append
                     $UnusedFileCount = $UnusedFileCount + 1
-                 }
+            }
 
-                $Error.Clear()
+            $Error.Clear()
 
-                $jpg | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue
-                If($Error[0] -notmatch "because it does not exist."){ 
+            If($blnMoveMorphs -eq $true){ $jpg | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue }
+            If($Error[0] -notmatch "because it does not exist."){ 
                     """"+$type+""""+','+""""+ $jpg +"""" | Out-File -FilePath $MoveReportCSVpath -Append
                     $UnusedFileCount = $UnusedFileCount + 1
-                 }
+            }
 
                 $Error.Clear()
 
-                $png | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue
-                If($Error[0] -notmatch "because it does not exist."){ 
+            If($blnMoveMorphs -eq $true){ $png | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue }
+            If($Error[0] -notmatch "because it does not exist."){ 
                     """"+$type+""""+','+""""+ $png +"""" | Out-File -FilePath $MoveReportCSVpath -Append
                     $UnusedFileCount = $UnusedFileCount + 1
-                 }
+            }
         
-            } # If blnMove Morphs = true
-
-        } # if .vmi
+        } # If vmi
 
         ElseIf($File_VAMpath -imatch '.jpg' -or $_.Link_RelPath -imatch '.png'){ 
 
@@ -259,21 +266,36 @@ $arrResourceFiles | ForEach-Object {
 
             If($SimilarFiles.Count -eq 0){
                             
-                Write-Host ---Moving image: $File_FullPath
+                Write-Host ---Orphaned image: $File_FullPath
                 """"+'Image'+""""+','+""""+$File_FullPath+"""" | Out-File -FilePath $MoveReportCSVpath -Append
                 $UnusedFileCount = $UnusedFileCount + 1
                 
-                If($blnMoveTextures -eq $true){ 
-                    ($File_FullPath) | Move-Item -Destination ($RecycleBin + "\Images")  -Force -ErrorAction SilentlyContinue
-                    If($Error[0] -notmatch "because it does not exist."){ 
+                If($blnMoveTextures -eq $true){ ($File_FullPath) | Move-Item -Destination ($RecycleBin + "\Images")  -Force -ErrorAction SilentlyContinue }
+                If($Error[0] -notmatch "because it does not exist."){ 
                       """"+"Image"+""""+','+""""+ $png +"""" | Out-File -FilePath $MoveReportCSVpath -Append
                         $UnusedFileCount = $UnusedFileCount + 1
-                    }
-                 }
-                
+                }
+                               
             }
        
         } # if jpg or png
+
+        ElseIf($File_VAMpath -ilike '*.mp3' -or $File_VAMpath -ilike '*.wav' -or $File_VAMpath -ilike '*.ogg'){ 
+
+            # Audio
+            write-host ---Orphaned Audio: $File_VAMpath           
+
+            $type = "Audio"
+            $Error.Clear()
+                
+            If($blnMoveAudio -eq $true){ $File_FullPath | Move-Item -Destination ($RecycleBin + "\" + $type) -Force -ErrorAction SilentlyContinue }
+            If($Error[0] -notmatch "because it does not exist."){ 
+                    """"+$type+""""+','+""""+ $File_FullPath +"""" | Out-File -FilePath $MoveReportCSVpath -Append
+                    $UnusedFileCount = $UnusedFileCount + 1
+            }
+
+        } # If Audio
+
         Else{
 
             <# Other types not managed:          
